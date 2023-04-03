42fc307c2a53a4351535bb44a5c98d4972236342
あき <118268728+aki-0421@users.noreply.github.com>
2023-04-03 17:36:41 +0900

    Setup Real-ESRGAN (#2)
    * C: add ci
    
    * R: fix
    
    * R: fix
    
    * R: fix

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,42 @@
-FROM python:3.11.1-buster
+FROM nvidia/cuda:11.6.1-cudnn8-devel-ubuntu18.04
 
-WORKDIR /
+# update
+ENV DEBIAN_FRONTEND noninteractive
+RUN apt-get update
+RUN apt-get install -y --no-install-recommends
+RUN apt-get install -y git software-properties-common
+COPY --from=ghcr.io/tarampampam/curl:8.0.1 /bin/curl /bin/curl
 
-RUN pip install git+https://github.com/runpod/runpod-python.git
+# setup python3.10
+RUN apt-get install -y python3.8 python3.8-distutils python3.8-dev python3.8-venv python3-pip
+
+# create venv
+RUN python3.8 -m venv /venv
+ENV PATH=/venv/bin:$PATH
+
+# clone Real-ESRGAN
+RUN git clone https://github.com/xinntao/Real-ESRGAN.git /usr/src/app
+
+WORKDIR /usr/src/app
+
+# upgrade pip
+RUN python3.8 -m pip install --upgrade pip==23.0.1
+# install runpod
+RUN python3.8 -m pip install runpod==0.9.1
+RUN python3.8 -m pip install cog
+# install Real-ESRGAN dependencies
+RUN apt-get install -y libgl1-mesa-dev
+RUN python3.8 -m pip install Cython
+RUN python3.8 -m pip install torch==1.13.1 --extra-index-url=https://download.pytorch.org/whl/cu116
+RUN python3.8 -m pip install basicsr
+RUN python3.8 -m pip install facexlib
+RUN python3.8 -m pip install gfpgan
+RUN python3.8 -m pip install -r requirements.txt
+RUN python3.8 setup.py develop
 
 ADD handler.py .
 ADD test_input.json .
 
-CMD [ "python", "-u", "/handler.py" ]
+RUN curl -L -O --create-dirs --output-dir weights https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth
+
+CMD [ "python3.8", "-u", "handler.py" ]
--- handler.py
+++ handler.py
@@ -1,19 +1,80 @@
 import runpod
+from runpod.serverless.utils import rp_download, rp_upload, rp_cleanup
+from runpod.serverless.utils.rp_validator import validate
+
 import os
-import time
+import requests
 
-sleep_time = int(os.environ.get('SLEEP_TIME', 3))
+import cv2
+from basicsr.archs.rrdbnet_arch import RRDBNet
 
-def handler(event):
-    print(event)
-    time_slept = 0
-    while time_slept < sleep_time:
-        print("working, I promise")
-        time_slept += 1
-        time.sleep(1)
-    # do the things
+from realesrgan import RealESRGANer
 
-    return "Hello World"
+INPUT_SCHEMA = {
+    'image_url': {
+        'type': str,
+        'required': True
+    },
+}
+
+
+def handler(job):
+    job_input = job['input']
+
+    # Input validation
+    validated_input = validate(job_input, INPUT_SCHEMA)
+
+    if 'errors' in validated_input:
+        return {"error": validated_input['errors']}
+    validated_input = validated_input['validated_input']
+
+    model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
+    netscale = 4
+    outscale = 4
+    model_path = "weights/RealESRGAN_x4plus.pth"
+
+    upsampler = RealESRGANer(
+        scale=netscale,
+        model_path=model_path,
+        dni_weight=None,
+        model=model,
+        tile=0,
+        tile_pad=10,
+        pre_pad=0,
+        half=False,
+        gpu_id=None)
+
+    # Download input objects
+    image_file_path = rp_download.download_input_objects(
+        [validated_input.get('image_url', None)]
+    )  # pylint: disable=unbalanced-tuple-unpacking
+    image = image_file_path[0]
+
+    imgname, extension = os.path.splitext(os.path.basename(image))
+    img = cv2.imread(image, cv2.IMREAD_UNCHANGED)
+    if len(img.shape) == 3 and img.shape[2] == 4:
+        img_mode = 'RGBA'
+    else:
+        img_mode = None
+
+
+    image_url = ""
+    try:
+        output, _ = upsampler.enhance(img, outscale=outscale)
+    except RuntimeError as error:
+        print('Error', error)
+        print('If you encounter CUDA out of memory, try to set --tile with a smaller number.')
+    else:
+        extension = extension[1:]
+        if img_mode == 'RGBA':  # RGBA images should be saved in png format
+            extension = 'png'
+        save_path = os.path.join("", f'{imgname}.{extension}')
+
+        cv2.imwrite(save_path, output)
+
+        image_url = rp_upload.upload_image(job['id'], save_path)
+
+    return image_url
 
 
 runpod.serverless.start({
